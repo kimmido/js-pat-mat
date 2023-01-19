6e2f71b3bd847d8876ce671e63e73362d2af97e0
kimmido <icecream120102@gmail.com>
2023-01-19 23:38:01 +0900

    메뉴1 완성, 배경음악 변경 스크립트 완료

--- js/index.js
+++ js/index.js
@@ -5,19 +5,17 @@ window.onload = function() {
     const musicCancel = document.getElementById('musicCancel');
     const musicModal = document.getElementById('musicModal');
 
-    let toggle = true; 
+    musicStart.addEventListener("click", function() {
+        toggle = false;
+        musicToggle();
+        musicModal.style.display = "none";
+    })
 
-    // musicStart.addEventListener("click", function() {
-    //     toggle = false;
-    //     musicToggle();
-    //     musicModal.style.display = "none";
-    // })
-
-    // musicCancel.addEventListener("click", function() {
-    //     toggle = true;
-    //     musicToggle();
-    //     musicModal.style.display = "none";
-    // })
+    musicCancel.addEventListener("click", function() {
+        toggle = true;
+        musicToggle();
+        musicModal.style.display = "none";
+    })
 
     musicControl.addEventListener("click", function() {
         this.style.transform = "scale(0.8)";
@@ -38,53 +36,84 @@ window.onload = function() {
         }
     }
 
+
+    const source = music.querySelector("source");
+
+    function changeAudio(audioName) {
+        if(!music.paused) {
+            source.setAttribute("src", `./media/Pat_Mat_${audioName}_music.mp3`)
+            music.load();
+            music.play();
+        }
+    }
+
     
     const menu = document.querySelectorAll(".menu");
     const menu1 = document.getElementById("menu1");
-    const menuCon1 = document.getElementById("menuCon1");
     
-    // menu1.getAttribute
+    console.log(menu);
 
     let img;
     let src;
     let val;
 
-    for(let i = 1; i > menu.length; i++) {
-        
+    for(let i = 0; i < menu.length; i++) {
         menu[i].addEventListener("mouseover", function() {
             img = this.querySelector("img");
             src = img.getAttribute("src");
-            val = src.replace(`menu(${i})_shadow`,`menu(${i})`);
+            val = src.replace(`menu(${i + 1})_shadow`,`menu(${i + 1})`);
             img.setAttribute("src", val);
         })
-
+        
         menu[i].addEventListener("mouseout", function() {
-            val = src.replace(`menu(${i})`,`menu(${i})_shadow`);
+            img = this.querySelector("img");
+            src = img.getAttribute("src");
+            val = src.replace(`menu(${i + 1})`,`menu(${i + 1})_shadow`);
             img.setAttribute("src", val);
         })
+        
     }
     
-    // menu1.addEventListener("mouseover", function() {
-    //     console.log(this.querySelector("img").getAttribute("src"));
-    //     let img = this.querySelector("img");
-    //     let src = img.getAttribute("src");
-    //     let val = src.replace("menu(1)_shadow","menu(1)");
-    //     img.setAttribute("src", val);
-        // this.children.
-        // this.querySelector("img");
-    // })
-    // menu1.addEventListener("mouseout")
 
+    const Con01 = document.getElementById("Con1");
     
     menu1.addEventListener("click", function() {
-        menuCon1.classList.add("on");
+        Con01.classList.add("on");
+        changeAudio('menu(1)');
     })
-
+    
+    
     const close = document.querySelectorAll(".close");
-
+    
     close.forEach(close => {
         close.onclick = function() {
             close.parentNode.classList.remove("on");
+            changeAudio('main');
         }
     });
+
+
+    const tab = Con01.querySelector(".tab");  
+    const tabConList = Con01.querySelectorAll(".tab-con-list");  
+    let idx;
+
+    tab.addEventListener("click", function(e) {
+        if(e.target.matches("li")) {
+            idx =[...this.children].indexOf(e.target);
+
+            [...this.children].forEach(li => {
+                li == e.target ?
+                li.classList.add("on") :
+                li.classList.remove("on")
+            });
+
+            tabConList.forEach(conList => {
+                [...conList.children].forEach(con => {
+                    con == [...conList.children][idx] ?
+                    con.classList.add("on") :
+                    con.classList.remove("on")
+                })
+            });
+        }
+    })
 }
